639427846a6e993015298ba664ed10783c73a289
Thiago <thiagobomfim@gmail.com>
2017-10-09 22:34:56 -0300

    Criacao do decode para o oauth2

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { Title } from '@angular/platform-browser';
 
 import { NavbarComponent } from './navbar/navbar.component';
-
 import { PessoaService } from '../pessoas/pessoa.service';
 import { LancamentoService } from '../lancamentos/lancamento.service';
 import { ConfirmationService } from 'primeng/components/common/confirmationservice';
 import { ErrorHandlerService } from './error-handler.service';
 import { CategoriaService } from '../categoria/categoria.service';
+import { AuthService } from '../seguranca/auth.service';
+
+import { JwtHelper } from 'angular2-jwt';
 
 import { ToastyModule } from 'ng2-toasty';
 import { ConfirmDialogModule } from 'primeng/components/confirmdialog/confirmdialog';
@@ -31,6 +33,8 @@ import { PaginaNaoEncontradaComponent } from './pagina-nao-encontrada.component'
     PessoaService,
     ConfirmationService,
     CategoriaService,
+    AuthService,
+    JwtHelper,
     Title,
     { provide: LOCALE_ID, useValue: 'pt-BR' }
   ]
--- src/app/seguranca/login-form/login-form.component.ts
+++ src/app/seguranca/login-form/login-form.component.ts
@@ -1,3 +1,4 @@
+import { AuthService } from './../auth.service';
 import { Component } from '@angular/core';
 
 @Component({
@@ -9,11 +10,11 @@ export class LoginFormComponent {
 
   usuario: string;
   senha: string;
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
 
   login() {
-
+    this.authService.login(this.usuario, this.senha);
   }
 
 }
