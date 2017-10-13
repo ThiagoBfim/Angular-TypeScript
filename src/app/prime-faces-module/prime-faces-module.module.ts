2854af10ed872174e114e9ba46250904b1af89df
Thiago <thiagobomfim@gmail.com>
2017-10-13 19:39:47 -0300

    Correcao do module do primeFaces

--- src/app/prime-faces-module/prime-faces-module.module.ts
+++ src/app/prime-faces-module/prime-faces-module.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { InputTextModule } from 'primeng/Components/inputtext/inputtext';
-import { ButtonModule } from 'primeng/Components/button/button';
-import { DataTableModule } from 'primeng/Components/datatable/datatable';
-import { TooltipModule } from 'primeng/Components/tooltip/tooltip';
-import { InputTextareaModule } from 'primeng/Components/inputtextarea/inputtextarea';
-import { CalendarModule } from 'primeng/Components/calendar/calendar';
-import { SelectButtonModule } from 'primeng/Components/selectbutton/selectbutton';
-import { DropdownModule } from 'primeng/Components/dropdown/dropdown';
-import { InputMaskModule } from 'primeng/Components/inputmask/inputmask';
+import { InputTextModule } from 'primeng/components/inputtext/inputtext';
+import { ButtonModule } from 'primeng/components/button/button';
+import { DataTableModule } from 'primeng/components/datatable/datatable';
+import { TooltipModule } from 'primeng/components/tooltip/tooltip';
+import { InputTextareaModule } from 'primeng/components/inputtextarea/inputtextarea';
+import { CalendarModule } from 'primeng/components/calendar/calendar';
+import { SelectButtonModule } from 'primeng/components/selectbutton/selectbutton';
+import { DropdownModule } from 'primeng/components/dropdown/dropdown';
+import { InputMaskModule } from 'primeng/components/inputmask/inputmask';
 
 
 @NgModule({
